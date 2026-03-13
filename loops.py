38410b47c761fe6414bea6c016093795e45d9ee1
monojoycodes <24deym@rbunagpur.in>
2026-03-13 23:08:23 +0530

    added till 13 JAV

--- loops.py
+++ loops.py
@@ -21,3 +21,85 @@ if days == "SATURDAY" or "SUNDAY":
 else: 
     print("not a weekend")"""
 
+#Day 3
+'''for i in range(1,6):
+    if i==3:
+        continue
+    print(i, " ", 6-i)
+
+# another way:
+for i, j in zip(range(1,6), range(5,0,-1)): # run multiple conditions- loop in once!
+    if i == 3: 
+        continue
+    print(i, j)
+
+# while
+i = 1
+while i<=5:
+    print(i)
+    i +=1
+'''
+name = "Monojoy"
+# o/p = "Monjy"
+newname= ""
+
+'''for i in name:
+    if (i in newname):
+        continue
+    newname = newname + i
+print(newname)
+
+for i in name:
+    if i not in name:
+        newname = newname + i
+print(newname)'''
+
+#Reverse a string using loop
+'''name = "Monojoy"
+#op = "yojonoM"
+name = list(name)
+
+for i, j in zip(range(0, len(name)), range(len(name)-1, 0, -1)):
+    temp = name[i]
+    name[i] = name[j]
+    name[j] = temp
+    
+    if i > j:
+        break
+    
+print(name)
+        
+# in string-
+reversed_name = ""
+for i in name:
+    reversed_name = reversed_name + i
+    print(reversed_name)
+'''
+
+# Palindromic 
+'''name = "SWAPNIL"
+if (name == name[::-1]):
+    print("PALINDROME")
+else:
+    print("not a palindrome")
+    '''
+
+#anagram
+word = "listen" #op = "silent"
+another_word = "monojo"
+word = list(word)
+another_word = list(another_word)
+
+word.sort()
+another_word.sort()
+
+for i in range(0, len(word)):
+    if (word[i] == another_word[i]):
+        flag = True
+    else: 
+        flag = False
+
+if flag:
+    print("ANAGRAM")
+else:
+    print("NOT ANAGRAM")
